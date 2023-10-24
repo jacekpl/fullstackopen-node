876025df45a00200d2532ccd1e511c839123c0cb
Jacek Różański <jacek@opcode.pl>
2023-10-24 23:57:16 +0200

    Preparation for 4.15

--- 4cd/controllers/blogs.js
+++ 4cd/controllers/blogs.js
@@ -1,13 +1,36 @@
 const blogsRouter = require('express').Router()
 const Blog = require("../models/blog");
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.startsWith('Bearer ')) {
+        return authorization.replace('Bearer ', '')
+    }
+    return null
+}
 
 blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog.find({}).populate('user', {username: 1, name: 1})
     response.json(blogs)
 })
 
-blogsRouter.post('/', async (request, response, next) => {
-    const blog = new Blog(request.body)
+blogsRouter.post('/', async (request, response) => {
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
+    const blog = new Blog(
+        {
+            "title": request.body.title,
+            "author": request.body.author,
+            "url": request.body.url,
+            "likes": request.body.likes,
+            "user": user.id
+        })
+
     if (!blog.likes) {
         blog.likes = 0
     }
@@ -17,6 +40,9 @@ blogsRouter.post('/', async (request, response, next) => {
     }
 
     const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+
     response.status(201).json(savedBlog);
 })
 
