d976cb0bd9667e4444c29c47af5db68b5941d206
Jacek Różański <jacek@opcode.pl>
2023-10-25 21:53:05 +0200

    Exercise 4.23

--- 4cd/controllers/blogs.js
+++ 4cd/controllers/blogs.js
@@ -2,13 +2,19 @@ const blogsRouter = require('express').Router()
 const Blog = require("../models/blog");
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
+const middleware = require("../utils/middleware");
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({}).populate('user', {username: 1, name: 1})
     response.json(blogs)
 })
 
-blogsRouter.post('/', async (request, response) => {
+blogsRouter.get('/:id', async (request, response) => {
+    const blog = await Blog.findById(request.params.id).populate('user', {username: 1, name: 1})
+    response.json(blog)
+})
+
+blogsRouter.post('/', middleware.userExtractor, async (request, response) => {
     const user = request.user
 
     const blog = new Blog(
@@ -35,7 +41,7 @@ blogsRouter.post('/', async (request, response) => {
     response.status(201).json(savedBlog);
 })
 
-blogsRouter.delete('/:id', async (request, response, next) => {
+blogsRouter.delete('/:id', middleware.userExtractor, async (request, response, next) => {
     const user = request.user
     const blog = await Blog.findById(request.params.id)
     if(!blog) {
@@ -50,7 +56,17 @@ blogsRouter.delete('/:id', async (request, response, next) => {
     response.status(204).end()
 })
 
-blogsRouter.put('/:id', async (request, response, next) => {
+blogsRouter.put('/:id', middleware.userExtractor, async (request, response, next) => {
+    const user = request.user
+    const blog = await Blog.findById(request.params.id)
+    if(!blog) {
+        return response.status(404).json({error: 'blog not found'})
+    }
+
+    if (blog.user.toString() !== user.id.toString()) {
+        return response.status(401).json({error: 'cannot update, you are not an author'})
+    }
+
     const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, request.body, {new: true})
     response.json(updatedBlog)
 })
--- 4cd/tests/blog_api.test.js
+++ 4cd/tests/blog_api.test.js
@@ -4,6 +4,9 @@ const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
 const Blog = require('../models/blog')
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 test('blogs are returned as json', async () => {
     await api
@@ -30,80 +33,147 @@ test('response contains id property', async () => {
     expect(response.body[0].id).toBeDefined()
 })
 
-test('a valid blog can be added ', async () => {
-    const newBlog = {
-        title: 'sample title',
-        author: 'blog author',
-        url: 'https://google.com',
-        likes: 2
-    }
+describe('blog actions requiring proper token', () => {
+    let token = null
 
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
+    beforeEach(async () => {
+        await User.deleteMany({})
+        const passwordHash = await bcrypt.hash('pass', 10)
+        const user = new User({ username: 'root', passwordHash })
+        await user.save()
+        token = jwt.sign({ username: user.username, id: user._id }, process.env.SECRET)
+    })
 
-    const inDb = await helper.blogsInDb()
-    expect(inDb.length).toBe(helper.initialBlogs.length + 1)
+    test('a valid blog can be added ', async () => {
+        const newBlog = {
+            title: 'sample title',
+            author: 'blog author',
+            url: 'https://google.com',
+            likes: 2
+        }
 
-    const contents = inDb.map(b => b.title)
-    expect(contents).toContain('sample title')
-})
+        await api
+            .post('/api/blogs')
+            .set('Authorization', 'bearer '+ token)
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
 
-test('if likes property is missing, it will default to 0', async () => {
-    const newBlog = {
-        title: 'sample title',
-        author: 'blog author',
-        url: 'https://google.com'
-    }
+        const inDb = await helper.blogsInDb()
+        expect(inDb.length).toBe(helper.initialBlogs.length + 1)
 
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
+        const contents = inDb.map(b => b.title)
+        expect(contents).toContain('sample title')
+    })
 
-    const response = await api.get('/api/blogs')
-    expect(response.body.length).toBe(helper.initialBlogs.length + 1)
+    test('add blog without token ', async () => {
+        const newBlog = {
+            title: 'sample title',
+            author: 'blog author',
+            url: 'https://google.com',
+            likes: 2
+        }
 
-    const latestBlog = response.body[response.body.length - 1]
-    expect(latestBlog.likes).toBe(0)
-})
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(401)
+    })
 
-test('if title or url properties are missing, it will return status 400', async () => {
-    const newBlog = {
-        author: 'blog author'
-    }
+    test('if likes property is missing, it will default to 0', async () => {
+        const newBlog = {
+            title: 'sample title',
+            author: 'blog author',
+            url: 'https://google.com'
+        }
 
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(400)
-})
+        await api
+            .post('/api/blogs')
+            .set('Authorization', 'bearer '+ token)
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
 
-test('delete blog', async () => {
-    await api
-        .delete(`/api/blogs/${helper.initialBlogs[0]._id}`)
-        .expect(204)
+        const response = await api.get('/api/blogs')
+        expect(response.body.length).toBe(helper.initialBlogs.length + 1)
 
-    const inDb = await helper.blogsInDb()
-    expect(inDb.length).toBe(helper.initialBlogs.length - 1)
-})
+        const latestBlog = response.body[response.body.length - 1]
+        expect(latestBlog.likes).toBe(0)
+    })
 
-test('update blog', async () => {
-    const newBlog = {
-        likes: 10,
-    }
+    test('if title or url properties are missing, it will return status 400', async () => {
+        const newBlog = {
+            author: 'blog author'
+        }
 
-    await api
-        .put(`/api/blogs/${helper.initialBlogs[0]._id}`)
-        .send(newBlog)
-        .expect(200)
+        await api
+            .post('/api/blogs')
+            .set('Authorization', 'bearer '+ token)
+            .send(newBlog)
+            .expect(400)
+    })
 
-    const response = await api.get('/api/blogs')
-    expect(response.body[0].likes).toBe(10)
-})
+    test('delete blog', async () => {
+        const newBlog = {
+            title: 'xyz',
+            author: 'blog author',
+            url: 'https://google.com',
+            likes: 2
+        }
+
+        const initialBlogsCount = await helper.blogsInDb()
+
+        await api
+            .post('/api/blogs')
+            .set('Authorization', 'bearer '+ token)
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+
+        const myBlog = await Blog.findOne({ title: 'xyz' })
+
+        const currentlyInDb = await helper.blogsInDb()
+        expect(currentlyInDb.length).toBe(initialBlogsCount.length + 1)
+
+        await api
+            .delete(`/api/blogs/${myBlog._id}`)
+            .set('Authorization', 'bearer '+ token)
+            .expect(204)
+
+        const inDb = await helper.blogsInDb()
+        expect(inDb.length).toBe(initialBlogsCount.length)
+    })
+
+    test('update blog', async () => {
+        const newBlog = {
+            title: 'xyz',
+            author: 'blog author',
+            url: 'https://google.com',
+            likes: 2
+        }
+        await api
+            .post('/api/blogs')
+            .set('Authorization', 'bearer '+ token)
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+
+        const updatedBlog = {
+            likes: 10,
+        }
+
+        const myBlog = await Blog.findOne({ title: 'xyz' })
+
+        await api
+            .put('/api/blogs/' + myBlog._id)
+            .set('Authorization', 'bearer '+ token)
+            .send(updatedBlog)
+            .expect(200)
+
+        const response = await api.get('/api/blogs/' + myBlog._id)
+        expect(response.body.likes).toBe(10)
+    })
+});
 
 beforeEach(async () => {
     await Blog.deleteMany({})
