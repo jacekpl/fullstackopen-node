dd9327b13cca6890bf7c9312e0dfba09a2eed247
Jacek Różański <jacek@opcode.pl>
2023-10-25 21:21:03 +0200

    Exercise 4.22

--- 4cd/controllers/blogs.js
+++ 4cd/controllers/blogs.js
@@ -9,11 +9,7 @@ blogsRouter.get('/', async (request, response) => {
 })
 
 blogsRouter.post('/', async (request, response) => {
-    const decodedToken = jwt.verify(request.token, process.env.SECRET)
-    if (!decodedToken.id) {
-        return response.status(401).json({error: 'token invalid'})
-    }
-    const user = await User.findById(decodedToken.id)
+    const user = request.user
 
     const blog = new Blog(
         {
@@ -40,17 +36,13 @@ blogsRouter.post('/', async (request, response) => {
 })
 
 blogsRouter.delete('/:id', async (request, response, next) => {
-    const decodedToken = jwt.verify(request.token, process.env.SECRET)
-    if (!decodedToken.id) {
-        return response.status(401).json({error: 'token invalid'})
-    }
-
+    const user = request.user
     const blog = await Blog.findById(request.params.id)
     if(!blog) {
         return response.status(404).json({error: 'blog not found'})
     }
 
-    if (blog.user.toString() !== decodedToken.id.toString()) {
+    if (blog.user.toString() !== user.id.toString()) {
         return response.status(401).json({error: 'cannot delete, you are not an author'})
     }
 
